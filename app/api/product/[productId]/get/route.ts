4c6ada01716845300e7d974c6b4f3a693600278f
tekenscodr <ensleybdarku@gmail.com>
2025-03-19 18:41:41 +0000

    updated return

--- app/api/product/[productId]/get/route.ts
+++ app/api/product/[productId]/get/route.ts
@@ -5,11 +5,12 @@ export async function GET(request: Request, context: any) {
     const { params } = context;
     const productId = await params.productId
     try {
-        return prisma.products.findFirst({
+        const product = prisma.products.findFirst({
             where: {
                 productID: productId
             }
         })
+        return NextResponse.json({ data: product, message: "Success" }, { status: 200 })
     } catch (error) {
         return NextResponse.json(error)
     }
